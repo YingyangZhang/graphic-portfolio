8b101f100c5d572b83b9f58fdd979fe0fa55df84
Yingyang Zhang <yingyang.zhang95@gmail.com>
2023-05-20 16:56:00 -0400

    added scroll restoration & icon

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,13 @@ import '../index.css';
 import Home from './Home/Home';
 import Inspection from './Inspection/Inspection';
 import { Route, Routes } from 'react-router-dom';
+import ScrollRestoration from './ScrollRestoration';
 
 export default function App() {
   return(
     <div className='app'>
+      <ScrollRestoration />
+
       <Routes>
         <Route path='/' element={ <Home /> } />
 
