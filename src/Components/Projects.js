4e2ff91af9cab1aade7975345489156a625ed955
Yingyang Zhang <yingyang.zhang95@gmail.com>
2024-01-04 14:03:13 -0500

    updated-haus-images

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -62,19 +62,22 @@ export const Projects = [
     {
         id: 4,
         name: 'Haus Furniture',
-        type: 'Web Design',
+        type: 'Branding Design',
         year: 2023,
         description: 'Haus Furniture is a minimalist website selling designer furniture. The black and white color scheme emphasizes quality and elegance. Captivating close-up images on the homepage showcase attention to detail. With a clean layout and user-friendly interface, customers can easily browse and purchase items.',
         images: {
-            cover_image: 'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/cover.jpg',
+            cover_image: 'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/businese-card.jpg',
             all_images: [
-                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+logo.jpg',
-                // 'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+gif.gif',
-                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+landing+page.jpg',
-                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+furnitures+page.jpg',
-                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+inspect+page.jpg',
-                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+cart.jpg',
-                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus/haus+checkout.jpg'
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/logo.jpg',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/businese-card.jpg',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/home.gif',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/collection.gif',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/piece.gif',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/archive.gif',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/billboard.jpg',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/truck.jpg',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/t-shirt.jpg',
+                'https://my-grapic-design-portfolio.s3.amazonaws.com/haus+furnitures/invoice.jpg'
             ]
         }
     },
